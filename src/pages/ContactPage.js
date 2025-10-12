e2aab4d2f404710b6ce30ec94319cbb1b0c01b7d
tylerfields56 <tylerfields56@gmail.com>
2025-10-12 03:17:28 -0400

    Add ContactPage with basic contact info

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ContactForm from '../components/ContactForm';
 
-function ContactPage() {
+const ContactPage = () => {
   return (
     <div>
-      <ContactForm />
+      <h1>Contact</h1>
+      <p>If you have any questions, feel free to reach out:</p>
+      <ul>
+        <li>Email: yourname@example.com</li>
+        <li>GitHub: <a href="https://github.com/yourusername" target="_blank" rel="noreferrer">yourusername</a></li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noreferrer">yourprofile</a></li>
+      </ul>
     </div>
   );
-}
+};
 
 export default ContactPage;
