d1fa8670c0f3bcf5cfbcd64e51e69c8e6f8efbeb
tylerfields56 <tylerfields56@gmail.com>
2025-10-12 03:12:13 -0400

    Add HomePage component with welcome message

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Task Manager</h1>
+      <p>This is your productivity hub. Navigate to the Todo section to manage your tasks efficiently, or reach out via the Contact page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
